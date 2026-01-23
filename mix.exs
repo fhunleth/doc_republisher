401c785ff9750279a24e7372fa81b32025339ca0
Frank Hunleth <fhunleth@troodon-software.com>
2026-01-23 09:01:20 -0500

    Add nerves_hub_link and fwup

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DocRepublisher.MixProject do
     [
       app: :doc_republisher,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
